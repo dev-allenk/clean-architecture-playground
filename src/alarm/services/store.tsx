f7ded58ad598e3246ff775ed6375d750d2314522
allenk <dev.allenk@gmail.com>
2022-07-31 11:21:35 +0900

    feat: Add User storage service

--- src/alarm/services/store.tsx
+++ src/alarm/services/store.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react"
 import { useContext } from "react"
-import { AlarmStorageService } from "../application/ports"
+import { AlarmStorageService, UserStorageService } from "../application/ports"
 import { Alarm } from "../domain/alarm"
+import { User } from "../domain/user"
 
-type ContextType = AlarmStorageService
+interface ContextType {
+  alarm: AlarmStorageService
+  user: UserStorageService
+}
 
 const StoreContext = React.createContext({} as ContextType)
 export const useStore = () => useContext(StoreContext)
 
 export function Provider({ children }: { children: React.ReactNode }) {
   const [alarms, setAlarms] = useState<Alarm[]>([])
+  const [user, setUser] = useState<User | null>(null)
 
   const value = {
-    alarms,
-    update: (newAlarms: Alarm[]) => setAlarms(newAlarms),
+    alarm: {
+      alarms,
+      update: (newAlarms: Alarm[]) => setAlarms(newAlarms),
+    },
+    user: {
+      user,
+      update: (newUser: User | null) => setUser(newUser),
+    },
   }
 
   return <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
