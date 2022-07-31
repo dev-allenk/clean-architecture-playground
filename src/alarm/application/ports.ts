3e94f3f4172c8e80e6a28e01581ae41d522dc2bc
allenk <dev.allenk@gmail.com>
2022-07-31 11:21:13 +0900

    feat: Add User ports

--- src/alarm/application/ports.ts
+++ src/alarm/application/ports.ts
@@ -1,4 +1,5 @@
 import { Alarm } from "../domain/alarm"
+import { User } from "../domain/user"
 
 export interface AlarmStorageService {
   alarms: Alarm[]
@@ -12,4 +13,14 @@ export interface NotificationService {
 export interface AlarmAPIService {
   tryAlarmSetting(alarm: Alarm): Promise<boolean>
   tryAlarmDelete(alarm: Alarm): Promise<boolean>
+}
+
+export interface UserStorageService {
+  user: User | null
+  update(user: User | null): void
+}
+
+export interface AuthAPIService {
+  login(email: string, password: string): Promise<User>
+  logout(): Promise<boolean>
 }
